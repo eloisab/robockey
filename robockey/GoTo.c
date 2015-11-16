9487477daad13e8e6fe14046457cffeaa237f4d3
Eloisa Baez Jones <eloisabaezjones@seas1062.wireless-pennnet.upenn.edu>
2015-11-16 02:50:55 -0500

    increasing library

--- robockey/GoTo.c
+++ robockey/GoTo.c
@@ -1,1 +1,10 @@
 // drive to given location
+// read in given location
+// read in current location/call localization
+// use drive commands to drive there
+
+// call localization
+
+// compute vector between two points
+
+// drive along vector
