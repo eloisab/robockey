81d4d25980900ac61fe628ff35b9ae15e29d64f1
Eloisa Baez Jones <eloisabaezjones@seas1062.wireless-pennnet.upenn.edu>
2015-11-16 02:57:34 -0500

    increasing library

--- robockey/main.c
+++ robockey/main.c
@@ -10,12 +10,17 @@
 #include "m_general.h"
 #include "m_bus.h"
 #include "m_rf.h"
+#include "m_wii.h"
+#include "LOCOlization.h"
 
 int main(void)
 {
+	char m_wii_open()
 
 	while(1)
 	{
+		m_wii_read(buffer);
+		char current_location(unsigned int* buffer)
 
 	}
 
