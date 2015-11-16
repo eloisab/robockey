77eda463685cb1b1940275d9a2e79e97bb4e1397
Eloisa Baez Jones <eloisabaezjones@seas1062.wireless-pennnet.upenn.edu>
2015-11-16 02:39:09 -0500

    pseudocode

---
+++ robockey/pseudocode.c
@@ -0,0 +1,21 @@
+// POSSIBLE STATES
+	// sees puck
+		// gets puck
+	// doesn't see puck
+		// searching
+	// driving without puck
+		// go fast
+	// driving with puck
+		// min turn radius
+		// drive slower
+// POSSIBLE ACTIONS
+	// goal scoring
+	// goal defending
+// POSSIBLE COMMUNICATIONS
+	// communicate states with other robots
+	
+// SEARCH PATH ALGORITHIM
+	// have a stack of locations to drive to
+
+
+// what does LOCOlization return
